380b566f0c1bd9064dd961b3b7463a44583f26f2
Graham Hukill <ghukill@gmail.com>
2016-07-29 01:39:42 -0400

    artistic updates

--- pyhw.py
+++ pyhw.py
@@ -5,6 +5,26 @@ print "Howdy."
 time.sleep(2)
 os.system('clear')
 print "We have assumed control of your computer.  Resistance is futile."
-time.sleep(10)
-print "Just kidding."
 
+time.sleep(3)
+
+print '''    ```'''
+time.sleep(.5)
+print '''    (`/\\'''
+time.sleep(.5)
+print '''    `=\/\\'''
+time.sleep(.5)
+print '''     `=\/\\'''
+time.sleep(.5)
+print '''      `=\/'''
+time.sleep(.5)
+print '''        _\___'''
+time.sleep(.5)
+print '''         ) ('''
+time.sleep(.5)
+print '''       ( INK )'''
+time.sleep(.5)
+print '''        \___/'''
+
+time.sleep(3)
+print "Just kidding.  All's well."
