9587d923da30cfde58d08e64feebe15122837f22
Graham Hukill <ghukill@gmail.com>
2016-07-29 01:21:52 -0400

    upday

--- pyhw.py
+++ pyhw.py
@@ -1,1 +1,10 @@
-print "Helllllllo World!"
+import os
+import time
+
+print "Howdy."
+time.sleep(2)
+os.system('clear')
+print "We have assumed control of your computer.  Resistance is futile."
+time.sleep(10)
+print "Just kidding."
+
