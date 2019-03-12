b2ca644ddeb25df1a44daade22ee56297344c9e1
Graham Hukill <ghukill@gmail.com>
2019-03-12 07:38:32 -0400

    py3

--- pyhw.py
+++ pyhw.py
@@ -1,30 +1,30 @@
 import os
 import time
 
-print "Howdy."
+print("Howdy.")
 time.sleep(2)
 os.system('clear')
-print "We have assumed control of your computer.  Resistance is futile."
+print("We have assumed control of your computer.  Resistance is futile.")
 
 time.sleep(3)
 
-print '''    ```'''
+print('''    ```''')
 time.sleep(.5)
-print '''    (`/\\'''
+print('''    (`/\\''')
 time.sleep(.5)
-print '''    `=\/\\'''
+print('''    `=\/\\''')
 time.sleep(.5)
-print '''     `=\/\\'''
+print('''     `=\/\\''')
 time.sleep(.5)
-print '''      `=\/'''
+print('''      `=\/''')
 time.sleep(.5)
-print '''        _\___'''
+print('''        _\___''')
 time.sleep(.5)
-print '''         ) ('''
+print('''         ) (''')
 time.sleep(.5)
-print '''       ( INK )'''
+print('''       ( INK )''')
 time.sleep(.5)
-print '''        \___/'''
+print('''        \___/''')
 
 time.sleep(3)
-print "Just kidding.  All's well."
+print("Just kidding.  All's well.  Thanks for playing.")
